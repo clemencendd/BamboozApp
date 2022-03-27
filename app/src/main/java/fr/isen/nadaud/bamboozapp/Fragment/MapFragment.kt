fe62c38f8f76920f720d8738d1dad391c7e8169a
Louise Courreges <louise.courreges@outlook.fr>
2022-03-27 20:18:12 +0200

    clean code

--- app/src/main/java/fr/isen/nadaud/bamboozapp/Fragment/MapFragment.kt
+++ app/src/main/java/fr/isen/nadaud/bamboozapp/Fragment/MapFragment.kt
@@ -15,41 +15,15 @@ import fr.isen.nadaud.bamboozapp.R
 import fr.isen.nadaud.bamboozapp.databinding.FragmentMapBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MapFragment : BamboozFragment(){
     lateinit var binding: FragmentMapBinding
     private lateinit var mMap: GoogleMap
-    var mMapView: MapView? = null
-
-    companion object {
-        var mapFragment : SupportMapFragment?=null
-        val TAG: String = MapFragment::class.java.simpleName
-        fun newInstance() = MapFragment()
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var rootView = inflater.inflate(R.layout.fragment_map, container, false)
-
-        /*val intent = Intent(activity, MapActivity::class.java)
-        (activity as MainActivity?)!!.startActivity(intent)*/
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
 
         // Initialize map fragment
         val supportMapFragment =
